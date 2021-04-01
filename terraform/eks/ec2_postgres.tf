3aaae65609e7a43cd1cfce379523a6b52618b608
Landung "Don" Setiawan <landungs@uw.edu>
2021-04-01 10:16:58 -0700

    Adding postgres instance tag

--- terraform/eks/ec2_postgres.tf
+++ terraform/eks/ec2_postgres.tf
@@ -5,6 +5,11 @@ resource "aws_instance" "postgres" {
   key_name               = "${var.hackweek_name}-postgres"
   subnet_id              = module.vpc.public_subnets[0]
   vpc_security_group_ids = [aws_security_group.postgres.id]
+    
+  tags = {
+    Name        = "Postgres machine for ${var.hackweek_name} hackweek"
+    Hackweek    = ${var.hackweek_name}
+  }
 
   root_block_device {
     volume_size           = 100
