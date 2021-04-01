c06d07ec7937f7263a2fab94f27975cd2f96d43c
Scott Henderson <scottyh@uw.edu>
2021-04-02 03:30:20 +1100

    Add EC2 instance for Postgres DB (#1)
    * initial config for postgres db
    
    * Use root block device only
    
    * Adding postgres port access to pods.
    
    * Use vpc cidr block
    
    Co-authored-by: Landung "Don" Setiawan <landungs@uw.edu>

---
+++ terraform/eks/ec2_postgres.tf
@@ -0,0 +1,41 @@
+resource "aws_instance" "postgres" {
+  # https://cloud-images.ubuntu.com/locator/ec2/
+  ami                    = "ami-0a62a78cfedc09d76"
+  instance_type          = "r5.2xlarge"
+  key_name               = "${var.hackweek_name}-postgres"
+  subnet_id              = module.vpc.public_subnets[0]
+  vpc_security_group_ids = [aws_security_group.postgres.id]
+
+  root_block_device {
+    volume_size           = 100
+    volume_type           = "gp2"
+    delete_on_termination = false
+  }
+
+}
+
+resource "aws_security_group" "postgres" {
+  name        = "${var.hackweek_name}-postgres"
+  description = "Allow SSH"
+  vpc_id      = module.vpc.vpc_id
+  ingress {
+    from_port = 5432
+    to_port   = 5432
+    protocol  = "tcp"
+    # Don't leave fully open for long periods of time?
+    cidr_blocks = [module.vpc.vpc_cidr_block]
+  }
+  ingress {
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+    # Don't leave fully open for long periods of time?
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
