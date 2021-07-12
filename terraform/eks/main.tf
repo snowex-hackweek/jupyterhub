dc27c382ccb91ed253de8a4af4e63e4929d50bb1
Landung "Don" Setiawan <landung.setiawan@gmail.com>
2021-07-12 12:42:46 -0700

    Update main.tf

--- terraform/eks/main.tf
+++ terraform/eks/main.tf
@@ -50,8 +50,8 @@ module "vpc" {
   cidr                 = "172.16.0.0/16"
   azs                  = data.aws_availability_zones.available.names
 
-  public_subnets       = ["172.16.1.0/24", "172.16.2.0/24", "172.16.3.0/24"]
-  private_subnets      = ["172.16.4.0/24", "172.16.5.0/24", "172.16.6.0/24"]
+  public_subnets       = ["172.16.0.0/19", "172.16.32.0/19", "172.16.64.0/19"]
+  private_subnets      = ["172.16.96.0/19", "172.16.128.0/19", "172.16.160.0/19"]
   enable_dns_hostnames = true
   enable_dns_support   = true
   enable_nat_gateway   = true
